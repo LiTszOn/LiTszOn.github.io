71594cfd128d016fb9aad1fd75e73a7d329ab48c
Tsz-On Li <u3518490@connect.hku.hk>
2026-08-21 09:40:54 +0800

    Fix dark/light toggle script loading and highlight theme

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -7,8 +7,8 @@
     document.body.classList.toggle('dark', !isDark);
     localStorage.setItem('wcTheme', isDark ? '0' : '1');
     var hl = document.querySelector('link[title="hl-light"]'), hd = document.querySelector('link[title="hl-dark"]');
-    if (hl) hl.disabled = isDark;
-    if (hd) hd.disabled = !isDark;
+    if (hl) hl.disabled = !isDark;
+    if (hd) hd.disabled = isDark;
     document.dispatchEvent(new CustomEvent('wcThemeChange', { detail: { isDarkTheme: function () { return !isDark; } } }));
   });
 })();
