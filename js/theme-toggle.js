c7c68308b2d2e444649137a749cffb474ea30ed1
Tsz-On Li <u3518490@connect.hku.hk>
2026-08-21 09:45:32 +0800

    Swap moon/sun icon with theme

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,6 +1,13 @@
 (function () {
   var $ = window.jQuery;
   if (!$) return;
+  function setIcon(isDark) {
+    var icon = document.querySelector('.js-dark-toggle i');
+    if (!icon) return;
+    icon.classList.toggle('fa-moon', !isDark);
+    icon.classList.toggle('fa-sun', isDark);
+  }
+  setIcon(document.body.classList.contains('dark'));
   $(document).on('click', '.js-dark-toggle', function (e) {
     e.preventDefault();
     var isDark = document.body.classList.contains('dark');
@@ -9,6 +16,7 @@
     var hl = document.querySelector('link[title="hl-light"]'), hd = document.querySelector('link[title="hl-dark"]');
     if (hl) hl.disabled = !isDark;
     if (hd) hd.disabled = isDark;
+    setIcon(!isDark);
     document.dispatchEvent(new CustomEvent('wcThemeChange', { detail: { isDarkTheme: function () { return !isDark; } } }));
   });
 })();
